a7b0387f8816cd0eb02c540bf00f99295200c28b
Michael Byers <mikebyers24@gmail.com>
2024-07-31 19:50:17 -0400

    added dictionary comprehension and a nato phonetic alphabet csv file. imported Pandas library.

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import pandas
-
 user_input = input("Enter a phrase: ").upper()
-
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-
 result = {row.letter: row for (index, row) in data.iterrows()}
-
 my_list = [result[letter].code for letter in user_input]
 print(my_list)
