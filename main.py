841169cd74cd78808bd5616d71e6624f9937edcc
Michael Byers <mikebyers24@gmail.com>
2024-07-31 19:55:30 -0400

    result loop refactor.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 user_input = input("Enter a phrase: ").upper()
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-result = {row.letter: row for (index, row) in data.iterrows()}
-my_list = [result[letter].code for letter in user_input]
+result = {row.letter: row.code for (index, row) in data.iterrows()}
+print(result)
+my_list = [result[letter] for letter in user_input]
 print(my_list)
