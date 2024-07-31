82e99cf4262c2b72e55374e35184910d94187205
Michael Byers <mikebyers24@gmail.com>
2024-07-31 19:55:54 -0400

    remove print statement.

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import pandas
 user_input = input("Enter a phrase: ").upper()
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 result = {row.letter: row.code for (index, row) in data.iterrows()}
-print(result)
 my_list = [result[letter] for letter in user_input]
 print(my_list)
