ec4345ef95f377dca883a694aeffc4e2bf30b1d5
Michael Byers <mikebyers24@gmail.com>
2024-08-06 08:44:58 -0400

    added error handling for KeyError.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pandas
-user_input = input("Enter a phrase: ").upper()
+
+is_going = True
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 result = {row.letter: row.code for (index, row) in data.iterrows()}
-my_list = [result[letter] for letter in user_input]
-print(my_list)
+
+while is_going:
+    user_input = input("Enter a phrase: ").upper()
+    try:
+        my_list = [result[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letter in the alphabet are allowed.")
+    else:
+        print(my_list)
+        is_going = False
+
